842ed1cc08318b75cd2f24ebe1f767f4ef464ce7
neomon <maryjoana@eclipso.me>
2016-04-23 16:34:59 +0200

    1. Added database

--- src/Shared/TimeTrackingPrototype.Shared.DataAccess/TimeTrackingContext.cs
+++ src/Shared/TimeTrackingPrototype.Shared.DataAccess/TimeTrackingContext.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimeTrackingPrototype.Shared.Entities;
 
-namespace TimeTrackingPrototype.Shared.DataAccess
-{
-    public class TimeTrackingContext
-    {
+namespace TimeTrackingPrototype.Shared.DataAccess {
+  public class TimeTrackingContext : DbContext {
+    const string DbConnectionStringName = "DbConnectionString";
+    const string DataDirectoryKey = "DataDirectory";
+
+    static readonly string DatabasePath;
+
     public DbSet<User> Users { get; set; }
     public DbSet<UserGroup> UserGroups { get; set; }
+
+
+    /// <summary>
+    /// Reads the database directory from the config file.
+    /// </summary>
+    static TimeTrackingContext() {
+      Debug.WriteLine("Reading database path from config file");
+      DatabasePath = ConfigurationManager.AppSettings[DataDirectoryKey];
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the database context class.
+    /// </summary>
+    public TimeTrackingContext() : base(DbConnectionStringName) {
+      string path = AppDomain.CurrentDomain.GetData(DataDirectoryKey) as String;
+      if (String.IsNullOrEmpty(path)) {
+        // Set |DataDirectory| value
+        AppDomain.CurrentDomain.SetData(DataDirectoryKey, DatabasePath);
+        Debug.WriteLine("DataDirectory set to: " + DatabasePath);
+      }
+
+      //Database.SetInitializer(new MigrateDatabaseToLatestVersion<TimeTrackingContext, Migrations.Configuration>(DbConnectionStringName));
+    }
+
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+      base.OnModelCreating(modelBuilder);
+    }
   }
 }
--- src/Shell/TimeTrackingPrototype.Shell.Wpf/App.xaml.cs
+++ src/Shell/TimeTrackingPrototype.Shell.Wpf/App.xaml.cs
@@ -5,11 +5,17 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TimeTrackingPrototype.Shared.DataAccess;
 
 namespace TimeTrackingPrototype.Shell.Wpf {
   /// <summary>
   /// Interaction logic for App.xaml
   /// </summary>
   public partial class App : Application {
+    private void Application_Startup(object sender, StartupEventArgs e) {
+      using (TimeTrackingContext ctx = new TimeTrackingContext()) {
+        MessageBox.Show("USER COUNT: " + ctx.Users.Count());
+      }
+    }
   }
 }
